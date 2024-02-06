ca800d1f40dc5abf694f9c78349d3c2aff0a82fd
Kayla Dale <kmeyers119@gmail.com>
2024-02-05 20:17:36 -0600

    added method for, greeting, getfile, assignment & anotherassigmnet

--- ReportCard.java
+++ ReportCard.java
@@ -7,31 +7,36 @@ import java.util.Scanner;
 
 public class ReportCard {
     public static void main(String... args) {
-        List<Student> students = new ArrayList<>();
+        List<Student> students = new ArrayList<>(); // [{kevin, dale} , {kayla, dale}]
         BufferedReader myReader = null;
         Scanner scan = new Scanner(System.in);
-        Scanner scanner = null;
+        Student student = null;
 
+        greeting(myReader, scan, student, students);
+    }
+
+    private static void greeting(BufferedReader myReader, Scanner scan, Student student, List<Student> students){
         System.out.println("Welcome to the Student Report Card Generator");
         System.out.println("--------------------------------------------\n");
-
+        getFile(myReader, scan, student, students);
+        assignment(scan, student, students);
+        anotherAssignment(scan, student, students);
+    }
+    private static void getFile(BufferedReader myReader,Scanner scan, Student student, List<Student> students){
         System.out.print("Enter the student file location: ");
         String file = scan.nextLine();
-
         try {
             myReader = new BufferedReader(new FileReader(file));
-            while (myReader.readLine() != null) {
-                String fullName = myReader.readLine();
+            String fullName;
+            while ((fullName = myReader.readLine()) != null) {
                 String firstName = fullName.split(" ")[0];
-                String lastName = fullName.split(" ")[0];
-                Student student = new Student();
-                student.setFirstName(firstName);
-                student.setLastName(lastName);
+                String lastName = fullName.split(" ")[1];
+                student = new Student(firstName, lastName);
                 students.add(student);
+                System.out.println("Creating student " + student.getFirstName() + " " + student.getLastName() + "\n");
             }
         } catch (Exception e) {
             System.out.println("error");
-
         } finally {
             try {
                 myReader.close();
@@ -40,5 +45,32 @@ public class ReportCard {
             }
         }
     }
+
+
+
+    private static void assignment(Scanner scan, Student student, List<Student> students) {
+        while (true) {
+            System.out.print("Enter the name of an Assignment: ");
+            String assignmentName = scan.nextLine();
+            for (Student stud : students) {
+                System.out.println("Enter grade for " + stud.getFirstName() + " " + stud.getLastName());
+                double grade = scan.nextDouble();
+                student.getAssignmentToGrade().put(assignmentName, grade);
+            }
+        }
+
+    }
+    private static void anotherAssignment(Scanner scan,Student student, List<Student> students){
+        System.out.print("Another assignment? (y/n): ");
+        String anotherAssignment=scan.nextLine();
+        if(anotherAssignment.equalsIgnoreCase("y")){
+            assignment(scan, student, students);
+        }else{
+            System.out.println("Tryagain");
+            anotherAssignment(scan, student, students);
+        }
+    }
+
+
 }
 //how to read scanner is 51:49
